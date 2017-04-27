297b3cc1d20ea42322924478fbf2a39e34fae6d2
Jason Choe <jasonkmchoe@gmail.com>
2017-04-27 16:32:57 -0400

    finished

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,20 @@
 'use strict';
 var express = require('express');
 var router = express.Router();
-var tweetBank = require('../tweetBank');
+var client = require('../db/index')
 
 module.exports = router;
 
 // a reusable function
-function respondWithAllTweets (req, res, next){
-  var allTheTweets = tweetBank.list();
-  res.render('index', {
-    title: 'Twitter.js',
-    tweets: allTheTweets,
-    showForm: true
+function respondWithAllTweets(req, res, next) {
+  client.query('SELECT * FROM tweets INNER JOIN users ON tweets.user_id = users.id', function (err, result) {
+    if (err) return next(err); // pass errors to Express
+    var tweets = result.rows;
+    res.render('index', {
+      title: 'Twitter-sql',
+      tweets,
+      showForm: true
+    });
   });
 }
 
@@ -20,30 +23,46 @@ router.get('/', respondWithAllTweets);
 router.get('/tweets', respondWithAllTweets);
 
 // single-user page
-router.get('/users/:username', function(req, res, next){
-  var tweetsForName = tweetBank.find({ name: req.params.username });
-  res.render('index', {
-    title: 'Twitter.js',
-    tweets: tweetsForName,
-    showForm: true,
-    username: req.params.username
+router.get('/users/:username', function (req, res, next) {
+  client.query('SELECT * FROM tweets INNER JOIN users ON tweets.user_id = users.id WHERE users.name = $1;', [req.params.username], function (err, result) {
+    if (err) return next(err); // pass errors to Express
+    var tweets = result.rows;
+    res.render('index', {
+      title: 'Twitter.js',
+      tweets,
+      showForm: true
+    });
   });
 });
 
 // single-tweet page
-router.get('/tweets/:id', function(req, res, next){
-  var tweetsWithThatId = tweetBank.find({ id: Number(req.params.id) });
-  res.render('index', {
-    title: 'Twitter.js',
-    tweets: tweetsWithThatId // an array of only one element ;-)
+router.get('/tweets/:id', function (req, res, next) {
+  client.query('SELECT * FROM tweets INNER JOIN users ON tweets.user_id = users.id WHERE tweets.id = $1', [req.params.id], function (err, result) {
+    if (err) return next(err);
+    var tweets = result.rows;
+    res.render('index', {
+      title: 'Twitter.js',
+      tweets
+    });
   });
 });
 
 // create a new tweet
-router.post('/tweets', function(req, res, next){
-  var newTweet = tweetBank.add(req.body.name, req.body.content);
-  res.redirect('/');
+router.post('/tweets', function (req, res, next) {
+  client.query('SELECT name FROM users WHERE name = $1', [req.body.name],
+    function (err, result) {
+      if (err) return next(err);
+      if (result.rows[0] == undefined) {
+        client.query('INSERT INTO users (name, picture_url) VALUES ($1, $2)', [req.body.name, 'http://i.imgur.com/XDjBjfu.jpg']);
+      }
+      client.query('INSERT INTO tweets (user_id, content) SELECT id, $1 FROM users WHERE name = $2', [req.body.content, req.body.name]);
+      res.redirect('/');
+    });
+
 });
+
+
+
 
 // // replaced this hard-coded route with general static routing in app.js
 // router.get('/stylesheets/style.css', function(req, res, next){
